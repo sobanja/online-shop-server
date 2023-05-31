8af108601fce50435f31ebfaf7c80456ecd8fe33
Ana Volenter <anavolenter1@gmail.com>
2023-05-31 19:27:23 +0300

    test2

--- server.js
+++ server.js
@@ -1,22 +1,10 @@
-const jsonServer = require('json-server');
-const tmp = require('tmp');
-const fs = require('fs');
-
-// Create a temporary directory
-const tmpDir = tmp.dirSync();
-
-// Create an empty db.json file in the temporary directory
-fs.writeFileSync(`${tmpDir.name}/db.json`, '{}');
-
-// Start JSON Server using the temporary db.json file
-const server = jsonServer.create();
-const router = jsonServer.router(`${tmpDir.name}/db.json`);
-const middlewares = jsonServer.defaults();
-
-server.use(middlewares);
-server.use(router);
-
-const port = 3000;
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
-});
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+ 
+server.use(middlewares)
+server.use('', router)
+server.listen(process.env.PORT || 5000, () => {
+  console.log('JSON Server is running')
+})
